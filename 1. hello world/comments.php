9b936dd9c836270023d02e72f9213d89de0f2b7c
Aman Mehta <mehta.aman@indiamart.com>
2021-08-28 00:16:47 +0530

    embedding php with html & writing comments in php

---
+++ 1. hello world/comments.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comments Demo</title>
+</head>
+<body>
+
+    <?php 
+      
+      //This is a single line comment
+
+      /* This is
+         example of
+         Multiline comment */
+
+    ?>
+
+    <h1>Comments Example in PHP</h1>
+    
+</body>
+</html>
